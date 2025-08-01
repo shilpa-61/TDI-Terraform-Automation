fc30afc8ebc88525964b5ce05021474b7f7c68e7
Sajan Nikam <sajannikam111@gmail.com>
2025-08-01 14:33:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-020cba7c55df1f615" 
 }
 variable "versioning" {
     type        = bool
