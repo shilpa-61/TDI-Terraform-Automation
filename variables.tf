9025aaf89ba237c4ee90a2d5318803a84935d407
shilpa-61 <shilpa.dataexpert@gmail.com>
2025-08-01 23:34:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "DevOps-Batch" 
+    default     =  "AMI" 
 }
 
 variable "instance_type" { 
