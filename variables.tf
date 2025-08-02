12c37325c34c489bde8c6f10cb1edc9e8364b5f8
Akash Makruwar <akashmakruwar1000@gmail.com>
2025-08-02 14:38:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "dev-default-sg" 
+    default     = "my_sg" 
 }
 
 variable "tag_name" { 
