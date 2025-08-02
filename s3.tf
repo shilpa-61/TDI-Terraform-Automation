5add9db60ea5549f4a4cd3f55ecc33b4163e2ead
Akash Makruwar <akashmakruwar1000@gmail.com>
2025-08-02 13:47:11 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,14 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
+  acl           = var.acl
+
   tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+
+  versioning_configuration {
+    status = var.versioning == true ? "Enabled" : "Suspended"
+  }
 }
