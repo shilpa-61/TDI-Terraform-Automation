59d804cee775f33391e7b3567a88a05b1a5bf624
shilpa-61 <shilpa.dataexpert@gmail.com>
2025-08-02 15:20:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "mydev-1-proj"
+    default     = "my-s3-bucket"
 }
 variable "tags" {
     type        = map
