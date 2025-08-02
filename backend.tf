caed13c50494ae934c1ec7423035991a6280120f
Akash Makruwar <akashmakruwar1000@gmail.com>
2025-08-02 12:38:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-terraform-project-akki-project-1"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "my-dynamodb_table-project1"
+    dynamodb_table = "my-dynamodb_table"
   }
 }
