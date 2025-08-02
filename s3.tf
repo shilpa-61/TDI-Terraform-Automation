e33506d298c00edba43249e2d2c686c270173708
shilpa-61 <shilpa.dataexpert@gmail.com>
2025-08-02 16:13:38 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,13 +1,11 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  tags          = var.tags
-}
-
-resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
-
-  versioning_configuration {
-    status = var.versioning == true ? "Enabled" : "Suspended"
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
   }
+  
+  tags = var.tags
 }
 
